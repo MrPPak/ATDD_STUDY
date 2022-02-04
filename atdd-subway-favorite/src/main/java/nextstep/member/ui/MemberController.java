2a4a8b21d48b51366c3a7fcd9ca5fdb895afa954
MrPPak <matht1115@naver.com>
2022-02-05 00:57:57 +0900

    test: TokenAuthenticationInterceptor TDD

---
+++ atdd-subway-favorite/src/main/java/nextstep/member/ui/MemberController.java
@@ -0,0 +1,67 @@
+package nextstep.member.ui;
+
+import nextstep.auth.context.SecurityContext;
+import nextstep.auth.context.SecurityContextHolder;
+import nextstep.member.application.MemberService;
+import nextstep.member.application.dto.MemberRequest;
+import nextstep.member.application.dto.MemberResponse;
+import nextstep.member.domain.LoginMember;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+
+import static nextstep.auth.context.SecurityContextHolder.SPRING_SECURITY_CONTEXT_KEY;
+
+@RestController
+public class MemberController {
+    private MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/members")
+    public ResponseEntity<Void> createMember(@RequestBody MemberRequest request) {
+        MemberResponse member = memberService.createMember(request);
+        return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity<MemberResponse> findMember(@PathVariable Long id) {
+        MemberResponse member = memberService.findMember(id);
+        return ResponseEntity.ok().body(member);
+    }
+
+    @PutMapping("/members/{id}")
+    public ResponseEntity<MemberResponse> updateMember(@PathVariable Long id, @RequestBody MemberRequest param) {
+        MemberResponse member = memberService.updateMember(id, param);
+        return ResponseEntity.ok().body(member);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<MemberResponse> deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/members/me")
+    public ResponseEntity<MemberResponse> findMemberOfMine(HttpServletRequest request) {
+        SecurityContext context = (SecurityContext) WebUtils.getSessionAttribute(request, SPRING_SECURITY_CONTEXT_KEY);
+        LoginMember loginMember = (LoginMember) context.getAuthentication().getPrincipal();
+        return ResponseEntity.ok(MemberResponse.of(loginMember));
+    }
+
+    @PutMapping("/members/me")
+    public ResponseEntity<MemberResponse> updateMemberOfMine() {
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/members/me")
+    public ResponseEntity<MemberResponse> deleteMemberOfMine() {
+        return ResponseEntity.noContent().build();
+    }
+}
+
