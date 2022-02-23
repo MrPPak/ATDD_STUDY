f195aded8e61f0071d95cd9e514389b748cce648
MrPPak <matht1115@naver.com>
2022-02-23 19:19:05 +0900

    atdd_orders 다른 프로젝트로 흡수

--- atdd-order/src/main/java/com/example/atddorder/application/CreateOrderService.java
+++
@@ -1,7 +0,0 @@
-package com.example.atddorder.application;
-
-import com.example.atddorder.domain.PendingOrder;
-
-public interface CreateOrderService {
-    PendingOrder createPendingOrder(PendingOrderRequest request);
-}
--- atdd-order/src/main/java/com/example/atddorder/domain/PendingOrderRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.atddorder.domain;
-
-public interface PendingOrderRepository {
-    PendingOrder save(PendingOrder pendingOrder);
-}
